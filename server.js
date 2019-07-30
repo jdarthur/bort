ec25c6e896091757fdb53979057571f761800fa9
jd <jd@laptop.lan>
2019-07-30 19:17:26 -0400

    database integration for register/login endpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const uuid = require("uuid/v4")
 const bcrypt = require("bcrypt")
+var mysql = require('mysql');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')))
@@ -10,76 +11,110 @@ app.use(express.json())
 
 const LEGAL_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXZY0123456789!@#$%^&"
 
-app.get('/ping', function (req, res) {
- return res.send({"response": "poong"})
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "bort"
 });
 
-let hash = null
-
+db.connect( err => {
+    if (err) {
+        throw err
+    }
+    console.log("connected to db")
+})
 
 app.post('/register', function (req, res) {
-    console.log(req.body)
-    if (req.body.username) {
-        const illch = has_illegal_characters(req.body.username)
-        if (illch !== false) {
-            return res.send({"response": "Illegal character '" + illch + "' in username."})
+    const valid_req = valid_login_register_request(req)
+    if (valid_req !== true) {
+        return res.send(valid_req)
+    }
+
+    db.query("SELECT * FROM user WHERE name='" + req.body.username + "';", (err, result) => {
+        if (err) {
+            throw_err
         }
-    }
-    else {
-        return res.send({"response": "Missing username field"})
-    }
 
-    if (req.body.password) {
-        const illch = has_illegal_characters(req.body.password)
-        if (illch !== false) {
-            return res.send({"response": "Illegal character '" + illch + "' in password."})
+        if (result.length === 0) {
+            const saltRounds = 10;
+            hash = bcrypt.hashSync(req.body.password, saltRounds)
+
+            db.query("INSERT INTO user (name, password_hash) values ('" + req.body.username + "', '" + hash + "');", (err2, result2) => {
+                if (err2) {
+                    return res.send({"response": "Failed to register user '" + req.body.username + "."})
+                }
+
+                return res.send({"response": "Successfully registered user '" + req.body.username + "' (" + hash + ")."})
+            })
+        } else {
+            return res.send({"response": "Failed to register user '" + req.body.username + "."})
         }
-    }
-    else {
-        return res.send({"response": "Missing password field"})
-    }
-
-    const saltRounds = 10;
-    hash = bcrypt.hashSync(req.body.password, saltRounds)
-
-
-    return res.send({"response": "Successfully registered user '" + req.body.username + "' (" + hash + ")."})
-
+    })
 });
 
 app.post('/login', function (req, res) {
-    console.log(req.body)
+    const valid_req = valid_login_register_request(req)
+    if (valid_req !== true) {
+        return res.send(valid_req)
+    }
+
+    db.query("SELECT * FROM user WHERE name='" + req.body.username + "';", (err, result) => {
+        if (err) {
+            throw_err
+        }
+
+        if (result.length !== 0) {
+            const hash = result[0].password_hash
+            const success = bcrypt.compareSync(req.body.password, hash)
+            if (success) {
+                 return res.send({"response": "Successfully logged in as '" + req.body.username + "."})
+            }
+            else {
+                return res.send({"response": "Invalid username or password."})
+            }
+        }
+        else {
+            return res.send({"response": "Invalid username or password."})
+        }
+    })
+});
+
+function valid_login_register_request(req) {
+    /*
+    Ensure login/registration request is not missing fields or
+    utilizing illegal characters.
+    */
+
     if (req.body.username) {
         const illch = has_illegal_characters(req.body.username)
         if (illch !== false) {
-            return res.send({"response": "Illegal character '" + illch + "' in username."})
+            return illegal_character_response(illch, "username")
         }
     }
     else {
-        return res.send({"response": "Missing username field"})
+        return missing_field_response("username")
     }
 
     if (req.body.password) {
         const illch = has_illegal_characters(req.body.password)
         if (illch !== false) {
-            return res.send({"response": "Illegal character '" + illch + "' in password."})
+            return illegal_character_response(illch, "password")
         }
     }
     else {
-        return res.send({"response": "Missing password field"})
+        return missing_field_response("username")
     }
+    return true
+}
 
-    const success = bcrypt.compareSync(req.body.password, hash)
-    if (success) {
-         return res.send({"response": "Successfully logged in as '" + req.body.username + "."})
-    }
-    else {
-        return res.send({"response": "Invalid username or password."})
-    }
-});
+function illegal_character_response(ill_char, field) {
+    return {"response": "Illegal character '" + ill_char + "' in " + field + "."}
+}
 
-
-
+function missing_field_response(field) {
+    return {"response": "Missing " + field + "' field."}
+}
 
 function has_illegal_characters(string) {
     // does username or password contain any illegal characters?
--- src/schemas.sql
+++ src/schemas.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS user (
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(32) UNIQUE,
     password_hash VARCHAR(64) NOT NULL,
-    password_salt VARCHAR(32) NOT NULL,
     privilege_level ENUM("user", "admin") DEFAULT "user",
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ) engine=innodb;
